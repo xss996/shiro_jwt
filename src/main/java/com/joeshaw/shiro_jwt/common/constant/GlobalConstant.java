ed501f6fb63e858d513b0867467921f69395c756
joeshaw <2551306415@qq.com>
2020-12-22 16:11:49 +0800

    shiro整合jwt实现登录,授权

---
+++ src/main/java/com/joeshaw/shiro_jwt/common/constant/GlobalConstant.java
@@ -0,0 +1,34 @@
+package com.joeshaw.shiro_jwt.common.constant;
+
+public class GlobalConstant {
+    /**
+     * 验证码过期时间 此处为五分钟
+     */
+    public static final Integer CODE_EXPIRE_TIME = 60 * 5;
+
+    /**
+     * jwtToken过期时间 默认为30天
+     * public static Integer TOKEN_EXPIRE_TIME = 7 * 24 * 60 * 60 * 1000;
+     */
+    public static final Long TOKEN_EXPIRE_TIME = 31 * 24 * 60 * 60 * 1000L;
+
+    /**
+     * UserId
+     */
+    public static final String USER_ID = "userId";
+
+
+    /**
+     * token请求头名称
+     */
+    public static final String TOKEN_HEADER_NAME = "token";
+
+    /*做token签名的字符串*/
+    public static final String TOKEN_SECRET="abc";
+
+    //token的载荷中盛放的信息 只盛放一个userId 其余什么也不再盛放
+    public static final String TOKEN_CLAIM="userId";
+
+    //redis存放用户验证码时给的前缀
+    public static final String REDIS_LOGIN_CODE="LOGIN_CODE:";
+}
